0a2eb4e55d4db78a9ac8bf41fa0ccfcd7dff4dc7
David Nunez <arizonatribe@gmail.com>
2022-04-12 10:10:18 -0600

    Added a hashtable implementation and test

---
+++ test/hashtable.test.js
@@ -0,0 +1,36 @@
+const assert = require("assert")
+const { HashTable } = require("../lib/hashtable")
+
+describe("Hash table tests", mocha => {
+    it("Getter and Setter", () => {
+        const ht = new HashTable()
+
+        ht.set("lorem", "ipsum")
+        ht.set("dolor", "sit amet")
+        ht.set("morel", "muspi")
+
+        const val = ht.get("lorem")
+
+        assert.equal(val, "ipsum")
+    })
+
+    it("Retrieve all keys", () => {
+        const ht = new HashTable()
+
+        ht.set("blue", "corn")
+        ht.set("star", "bucks")
+        ht.set("coffee", "trader")
+
+        assert.deepEqual(ht.keys(), ["coffee", "star", "blue"])
+    })
+
+    it("Retrieve all values", () => {
+        const ht = new HashTable()
+
+        ht.set("blue", "corn")
+        ht.set("star", "bucks")
+        ht.set("coffee", "trader")
+
+        assert.deepEqual(ht.values(), ["trader", "bucks", "corn"])
+    })
+})
